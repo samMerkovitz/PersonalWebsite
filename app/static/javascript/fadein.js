7ca883eac060ffc42d0eab67893de31daedc8ea7
samMerkovitz <s.merkovitz@gmail.com>
2020-08-25 19:36:43 -0700

    added mobile sidenav

--- app/static/javascript/fadein.js
+++ app/static/javascript/fadein.js
@@ -1,16 +1,6 @@
 
 
-    let tabPanes = document.getElementsByClassName("tab-header")[0].getElementsByTagName("div");
 
-    for(let i=0;i<tabPanes.length;i++){
-    tabPanes[i].addEventListener("click",function(){
-        document.getElementsByClassName("tab-header")[0].getElementsByClassName("active")[0].classList.remove("active");
-        tabPanes[i].classList.add("active");
-        
-        document.getElementsByClassName("tab-content")[0].getElementsByClassName("active")[0].classList.remove("active");
-        document.getElementsByClassName("tab-content")[0].getElementsByTagName("div")[i].classList.add("active");
-    })
-    }
 
 
     $(document).ready(function(){
@@ -47,5 +37,11 @@
       });
 
 
-
+      function openNav() {
+        document.getElementById("mySideNav").style.width = "250px";
+      }
       
+      /* Set the width of the side navigation to 0 */
+      function closeNav() {
+        document.getElementById("mySideNav").style.width = "0";
+      }
